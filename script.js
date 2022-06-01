212eb9d0e1701ce86149b12e71f604cb7cd0a471
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-01 11:45:07 -0700

    Scale new grid to size of container

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector('.container');
 container.innerHTML = '';
 const HEIGHT = 16;
 const WIDTH = 16;
+const LIMIT = 100;
+const GRIDSIZE = 960;
 
 for(let i = 0; i < HEIGHT * WIDTH; i++) {
     const box = document.createElement('div');
@@ -13,10 +15,11 @@ const button = document.querySelector('.change');
 button.addEventListener('click', () => {
     let newSize = prompt('How many boxes?');
     if((typeof newSize === 'number')) newSize = 16;
-    if(newSize > 100) newSize = 100;
+    if(newSize > LIMIT) newSize = LIMIT;
     if(newSize < 1) newSize = 1;
     const newHeight = newSize;
     const newWidth = newSize;
+    const scale = GRIDSIZE / newSize;
 
     for(let i = 0; i < HEIGHT * WIDTH; i++){
         var child = container.lastElementChild; 
@@ -29,6 +32,8 @@ button.addEventListener('click', () => {
     for(let i = 0; i < newHeight * newWidth; i++) {
         const box = document.createElement('div');
         box.classList = 'box';
+        box.style.height = `${scale}px`;
+        box.style.width = `${scale}px`;
         container.appendChild(box);
     }
     
